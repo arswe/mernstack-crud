ca23a9e34208e77a763cb8f1f6293e0fd97e23b9
Abdur Rahman <arwithlpd@gmail.com>
2023-01-05 15:22:04 +0600

    model add

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const connectDB = require('./database/db');
 
 const port = process.env.PORT || 5000;
 
+dotenv.config();
 connectDB();
 
 const app = express();
@@ -13,5 +14,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, () => {
-  console.log(colors.rainbow(`Server is running on port http://localhost:${port}`));
+  console.log(colors.cyan.bold(`Server is running on port http://localhost:${port}`));
 });
--- backend/database/db.js
+++ backend/database/db.js
@@ -5,12 +5,9 @@ mongoose.set('strictQuery', false);
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    console.log(colors.cyan.bold(`database connection successful!: ${conn.connection.host}`));
+    console.log(colors.rainbow(`database connection successful!: ${conn.connection.host}`));
   } catch (error) {
     console.error(colors.underline.bold(`Error: ${error.message}`));
     process.exit(1);
