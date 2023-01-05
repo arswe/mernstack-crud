7a683d8f71df8ed6a67de3226404710308a85549
Abdur Rahman <arwithlpd@gmail.com>
2023-01-05 13:35:26 +0600

    database connection

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 
-const port = process.env.PORT || 5000;
-
 const app = express();
 
 app.listen(process.env.PORT, () => {
