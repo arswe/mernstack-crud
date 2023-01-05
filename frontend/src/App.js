300735eb6688e1ddada31f54e5bdc51ec26f602f
Abdur Rahman <arwithlpd@gmail.com>
2023-01-05 15:38:41 +0600

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Box, Button, IconButton, Toolbar, Typography } from '@mui/material';
+import React from 'react';
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton size='large' edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+            MERNSTACK CRUD
+          </Typography>
+          <Button color='inherit'>Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
