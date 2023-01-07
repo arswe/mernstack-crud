640243ba35d818f8f0b8d379e139f2a703facc55
Abdur Rahman <arwithlpd@gmail.com>
2023-01-07 16:06:22 +0600

    appbar add

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,20 @@
+import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Box, Button, IconButton, Toolbar, Typography } from '@mui/material';
+
+export default function Navbar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton size='large' edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+            MERNSTACK CRUD
+          </Typography>
+          <Button color='inherit'>Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
