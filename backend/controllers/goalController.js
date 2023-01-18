11be12a595b986a28d7481ca60012dbcb05ce7f4
Abdur Rahman <arwithlpd@gmail.com>
2023-01-18 20:52:03 +0600

    controllers

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,3 +1,33 @@
+// @route   GET api/goals
+// @desc    Get all goals
+// @access  Public
+
 const getGoals = async (req, res) => {
   res.status(200).json({ success: true, msg: 'Show all goals' });
 };
+
+// @route   POST api/goals
+// @desc    Add a goal
+// @access  Public
+
+const addGoal = async (req, res) => {
+  res.status(200).json({ success: true, msg: 'Add a goal' });
+};
+
+// @route   PUT api/goals/:id
+// @desc    Update a goal
+// @access  Public
+
+const updateGoal = async (req, res) => {
+  res.status(200).json({ success: true, msg: `Update goal ${req.params.id}` });
+};
+
+// @route   DELETE api/goals/:id
+// @desc    Delete a goal
+// @access  Public
+
+const deleteGoal = async (req, res) => {
+  res.status(200).json({ success: true, msg: `Delete goal ${req.params.id}` });
+};
+
+module.exports = { getGoals, addGoal, updateGoal, deleteGoal };
