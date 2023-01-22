b7ae0003c658b6cd2ae847041e3757ec1b31d2b9
Abdur Rahman <arwithlpd@gmail.com>
2023-01-22 19:01:45 +0600

    udate

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <div>
       <h2>Navbar</h2>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit, veniam.</p>
+     
     </div>
   );
 }
